c870a6902e21e47909847fcbb31584c794a2b1f2
Ashish Walia <ashish4walia@gmail.com>
2025-06-09 13:44:43 +0530

    update dependencies and SDK versions; refactor build.gradle and pubspec.yaml for compatibility

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\tools\flutter3106"
-export "FLUTTER_APPLICATION_PATH=C:\Users\ashiw\work\test\google_map_location_picker\example"
+export "FLUTTER_ROOT=/Users/drimsusunepjamir/flutter/flutter_mor"
+export "FLUTTER_APPLICATION_PATH=/Users/drimsusunepjamir/work/samples/google_map_location_picker/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1.0.0"
